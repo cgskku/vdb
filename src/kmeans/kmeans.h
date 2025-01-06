647071860466f12d66be0e4c9da534ee76fba0aa
MinhoBaek <hoeo456@g.skku.edu>
2025-01-06 20:46:54 +0900

    split dim_test.cu, dim_chunk.cu

--- src/kmeans/kmeans.h
+++ src/kmeans/kmeans.h
@@ -5,9 +5,10 @@
 extern "C" {
 #endif
 
-void launch_kmeans_labeling(float *d_datapoints, int *d_clust_assn, float *d_centroids, int N, int TPB, int K, int dimension, int chunkSize);
-void launch_kmeans_update_center(float *d_datapoints, int *d_clust_assn, float *d_partial_centroids, int *d_partial_clust_sizes, int N, int TPB, int K, int dimension, int chunkSize);
-
+void launch_kmeans_labeling(float *d_datapoints, int *d_clust_assn, float *d_centroids, int N, int TPB, int K, int dimension);
+void launch_kmeans_update_center(float *d_datapoints, int *d_clust_assn, float *d_partial_centroids, int *d_partial_clust_sizes, int N, int TPB, int K, int dimension);
+void launch_kmeans_labeling_chunk(float *d_datapoints, int *d_clust_assn, float *d_centroids, int N, int TPB, int K, int dimension, int chunkSize);
+void launch_kmeans_update_center_chunk(float *d_datapoints, int *d_clust_assn, float *d_partial_centroids, int *d_partial_clust_sizes, int N, int TPB, int K, int dimension, int chunkSize);
 #ifdef __cplusplus
 }
 #endif
